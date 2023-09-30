24d4a3ab9477b46129cceb42b82576c010114ed1
Priyansu8603 <priyanshukumar238202@gmail.com>
2023-09-30 21:05:48 +0530

    added splash screen

--- app/src/main/java/com/example/notesapp/MainActivity.kt
+++ app/src/main/java/com/example/notesapp/MainActivity.kt
@@ -1,11 +1,36 @@
 package com.example.notesapp
 
+import android.content.Intent
+import android.os.AsyncTask
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        startHeavyTask()
+    }
+    private fun startHeavyTask() {
+        LongOperation().execute()
+    }
+    private open inner class LongOperation : AsyncTask<String?, Void, String?>() {
+        override fun doInBackground(vararg p0: String?): String? {
+            for (i in 0..5) {
+                try {
+                    Thread.sleep(1000)
+                } catch (e: Exception) {
+                    Thread.interrupted()
+                }
+            }
+            return "result"
+        }
+
+        override fun onPostExecute(result: String?) {
+            super.onPostExecute(result)
+            val intent = Intent(this@MainActivity, homepage::class.java)
+            startActivity(intent)
+        }
     }
 }
